c5e6d6695cfb9c2f9b26b9064ae4d6b61c8c8988
edith <jirunjia@gmail.com>
2021-06-02 20:40:13 +0800

    Add license and setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name='magcluster',
+    version='0.0.2',
+    description="Magnetosome gene cluster annotation, screening and mapping tool",
+    url="https://github.com/RunJiaJi/magcluster",
+    author='Runjia Ji',
+    author_email='jirunjia@gmail.com',
+    py_modules=["magcluster"],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "clinker",
+        'pandas',
+    ]
+    entry_points={
+        'console_scripts':[
+            'magcluster = magcluster:main'
+        ]
+    }
+)
